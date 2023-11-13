680e132d272f9d009be1fa84c4ce7d4ded1c1f5f
Won SeungBin <wsb0514@naver.com>
2023-11-13 15:58:21 +0900

    [SETTING] `SDK` 버전 및 `JDK` 버전 업데이트
    MIN SDK 버전 - 26
    JDK 버전 - 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.boostcampwm2023.snappoint"
-        minSdk = 28
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -28,8 +28,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "1.8"
