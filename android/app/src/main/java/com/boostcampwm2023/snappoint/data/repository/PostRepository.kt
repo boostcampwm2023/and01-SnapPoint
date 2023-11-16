11350a7d0b6ae3d1695b9e628f415d8db1f25a9a
Won SeungBin <wsb0514@naver.com>
2023-11-16 13:43:28 +0900

    [FEAT] `PostRepository` 게시글 작성 메서드 선언

--- android/app/src/main/java/com/boostcampwm2023/snappoint/data/repository/PostRepository.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/data/repository/PostRepository.kt
@@ -1,7 +1,10 @@
 package com.boostcampwm2023.snappoint.data.repository
 
+import com.boostcampwm2023.snappoint.presentation.createpost.PostBlock
 import kotlinx.coroutines.flow.Flow
 
 interface PostRepository {
     fun getImage(uri: String): Flow<ByteArray>
+    fun getImageUri(image: ByteArray): Flow<Unit>
+    fun postPost(postBlocks: List<PostBlock>): Flow<Unit>
 }
--- android/app/src/main/java/com/boostcampwm2023/snappoint/data/repository/PostRepositoryImpl.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/data/repository/PostRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.boostcampwm2023.snappoint.data.repository
 
 import com.boostcampwm2023.snappoint.data.remote.SnapPointApi
+import com.boostcampwm2023.snappoint.presentation.createpost.PostBlock
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.map
@@ -14,5 +15,19 @@ class PostRepositoryImpl @Inject constructor(
             .map{
             byteArrayOf()
         }
+    }
+
+    override fun getImageUri(image: ByteArray): Flow<Unit> {
+        return flowOf(true)
+            .map{
+
+            }
+    }
+
+    override fun postPost(postBlocks: List<PostBlock>): Flow<Unit> {
+        return flowOf(true)
+            .map{
+
+            }
     }
 }
