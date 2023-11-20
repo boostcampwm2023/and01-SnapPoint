11cbb603db3f2a129e7797ea1c5947a8b1e1b364
LJG7123 <ljg7123@naver.com>
2023-11-20 22:07:29 +0900

    [FIX] `AroundViewModel` `@HiltViewModel` 어노테이션 추가

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/around/AroundViewModel.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/around/AroundViewModel.kt
@@ -1,12 +1,14 @@
 package com.boostcampwm2023.snappoint.presentation.around
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
+@HiltViewModel
 class AroundViewModel @Inject constructor() : ViewModel() {
 
     private val _uiState: MutableStateFlow<AroundUiState> = MutableStateFlow(AroundUiState())
