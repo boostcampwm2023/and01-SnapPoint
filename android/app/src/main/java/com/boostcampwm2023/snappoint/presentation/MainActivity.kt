36e9fceeb5cb0dfbbfb5b929206e5d09db17816b
rbybound <ruviiit@gmail.com>
2023-11-20 15:53:17 +0900

    [FEAT] 지도 연결 및 bottom sheet 생성

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/MainActivity.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.boostcampwm2023.snappoint.presentation
 
+import android.os.Bundle
 import com.boostcampwm2023.snappoint.R
 import com.boostcampwm2023.snappoint.databinding.ActivityMainBinding
 import com.boostcampwm2023.snappoint.presentation.base.BaseActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main),
+    OnMapReadyCallback
+{
+    private var _map: GoogleMap? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val map: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.fcv_main_map) as SupportMapFragment
+        map.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        _map = googleMap
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(0.0,0.0), 17.5f))
+    }
+
 
 }
