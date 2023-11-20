c7baa4df0c1278429ebfa73b5793495891537a1b
rbybound <ruviiit@gmail.com>
2023-11-20 23:24:54 +0900

    [DESIGN] 미리보기 화면 rcv item 디자인

---
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/preview/PreviewViewHolder.kt
@@ -0,0 +1,17 @@
+package com.boostcampwm2023.snappoint.presentation.preview
+
+import android.net.Uri
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.boostcampwm2023.snappoint.databinding.ItemImagePreviewBinding
+
+class PreviewViewHolder(private val binding: ItemImagePreviewBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(uri: Uri, description: String) {
+        with(binding){
+            ivImagePreview.load(uri)
+            tvImageDescription.text = description
+        }
+    }
+}
