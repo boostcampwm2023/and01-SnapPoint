a0718a7fd3bef21c95f4791e2feffd89f477c16b
rbybound <ruviiit@gmail.com>
2023-11-21 18:07:15 +0900

    [FEAT] MainActivity에서 데이터 가져오기

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/preview/PreviewFragment.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/preview/PreviewFragment.kt
@@ -1,20 +1,57 @@
 package com.boostcampwm2023.snappoint.presentation.preview
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.boostcampwm2023.snappoint.R
 import com.boostcampwm2023.snappoint.databinding.FragmentPreviewBinding
 import com.boostcampwm2023.snappoint.presentation.base.BaseFragment
+import com.boostcampwm2023.snappoint.presentation.main.MainViewModel
+import com.google.android.material.carousel.CarouselSnapHelper
+import kotlinx.coroutines.launch
 
 class PreviewFragment : BaseFragment<FragmentPreviewBinding>(R.layout.fragment_preview) {
 
-    private val viewModel: PreviewViewModel by viewModels()
+    private val previewViewModel: PreviewViewModel by viewModels()
+    private val mainViewModel: MainViewModel by activityViewModels()
+
+    private val snapHelper: CarouselSnapHelper = CarouselSnapHelper()
+    private val layoutManager: LayoutManager by lazy { binding.rcvPreview.layoutManager!! }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.vm = viewModel
-        binding.rcvPreview.adapter = PreviewAdapter(viewModel.uiState.value.blocks)
+        initBinding()
+        collectViewModelData()
+
+        binding.rcvPreview.setOnScrollChangeListener { _, _, _, _, _ ->
+            Log.d(
+                "LOG",
+                "IDX: ${layoutManager.getPosition(snapHelper.findSnapView(layoutManager)!!)}"
+            )
+        }
+    }
+
+    private fun initBinding() {
+        with(binding) {
+            vm = previewViewModel
+            snapHelper.attachToRecyclerView(rcvPreview)
+        }
+    }
+
+    private fun collectViewModelData() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED){
+                mainViewModel.uiState.collect{
+                    previewViewModel.updatePost(it.posts[0])
+                }
+            }
+        }
     }
 }
