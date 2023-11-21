cc7744a6a8adcb2e1860ecc2bd2689638361830b
rbybound <ruviiit@gmail.com>
2023-11-21 22:48:49 +0900

    [FEAT] 상단바 BindingAdapter 추가

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/mainbindingAdapter.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/mainbindingAdapter.kt
@@ -1,7 +1,10 @@
 package com.boostcampwm2023.snappoint.presentation.main
 
+import android.annotation.SuppressLint
 import android.widget.LinearLayout
 import androidx.databinding.BindingAdapter
+import com.boostcampwm2023.snappoint.R
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.search.SearchBar
 
@@ -17,4 +20,21 @@ fun bindSearchBar(view: SearchBar, event: () -> Unit){
 fun LinearLayout.setDraggable(value: Boolean) {
     val behavior = BottomSheetBehavior.from(this)
     behavior.isDraggable = value
+}
+
+@BindingAdapter("navigation_icon_click")
+fun navigationIconClick(view: MaterialToolbar, event: () -> Unit) {
+    view.setNavigationOnClickListener {
+        event.invoke()
+    }
+}
+
+@BindingAdapter("menu_item_click")
+fun menuItemClick(view: MaterialToolbar, event: () -> Unit) {
+    view.setOnMenuItemClickListener {
+        when(it.itemId) {
+            R.id.preview_close -> event.invoke()
+        }
+        true
+    }
 }
