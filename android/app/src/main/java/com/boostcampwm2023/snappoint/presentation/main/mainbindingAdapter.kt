92fe0b9af8d8ef09102df0c9a5146d9a93a0cbb6
rbybound <ruviiit@gmail.com>
2023-11-21 21:36:18 +0900

    [FEAT] LinearLayout에 `draggable` BindingAdapter 추가

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/mainbindingAdapter.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/mainbindingAdapter.kt
@@ -1,6 +1,8 @@
 package com.boostcampwm2023.snappoint.presentation.main
 
+import android.widget.LinearLayout
 import androidx.databinding.BindingAdapter
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.search.SearchBar
 
 
@@ -9,4 +11,10 @@ fun bindSearchBar(view: SearchBar, event: () -> Unit){
     view.setNavigationOnClickListener {
         event.invoke()
     }
+}
+
+@BindingAdapter("draggable")
+fun LinearLayout.setDraggable(value: Boolean) {
+    val behavior = BottomSheetBehavior.from(this)
+    behavior.isDraggable = value
 }
