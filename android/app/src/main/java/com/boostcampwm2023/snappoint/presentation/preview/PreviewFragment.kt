1a6ca824336f94c407d2fc57a3d49f2219708aee
rbybound <ruviiit@gmail.com>
2023-11-21 14:25:33 +0900

    [FIX] 사진 비율 조정해서 뷰에 맞추기
    빈 공간은 사라지지만 비율이 이상해지는 단점이 있습니다.

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/preview/PreviewFragment.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/preview/PreviewFragment.kt
@@ -18,6 +18,16 @@ class PreviewFragment : BaseFragment<FragmentPreviewBinding>(R.layout.fragment_p
     private val timeStamp: String = "0 Days Ago"
     private val list: MutableList<PostBlockState> = mutableListOf(
         PostBlockState.IMAGE(
+            "세로로 길쭉해요!",
+            Uri.parse("https://gist.github.com/rbybound/0e96e32b94fb721fe9f0c563f4ff0ee7/raw/27325596c284c204fd2f539ee44a4da93a929aa7/3-2-1-height.jpg"),
+            PositionState(37.3586926, 127.1051209)
+        ),
+        PostBlockState.IMAGE(
+            "쭈우우우우우우우욱~!",
+            Uri.parse("https://gist.github.com/rbybound/0e96e32b94fb721fe9f0c563f4ff0ee7/raw/27325596c284c204fd2f539ee44a4da93a929aa7/3-2-1-width.jpg"),
+            PositionState(37.3586926, 127.1051209)
+        ),
+        PostBlockState.IMAGE(
             "aaas dfad fa df aaasd fad fas df aaa sdfa dfasdf aaasd fadf asdf ",
             Uri.parse("https://gist.github.com/rbybound/0e96e32b94fb721fe9f0c563f4ff0ee7/raw/9410800afbdbe648274523b68e8311308a46c37a/3-1-0.jpg"),
             PositionState(37.3586926, 127.1051209)
