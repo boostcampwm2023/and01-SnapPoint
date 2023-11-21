9e9ca6ce6f912e5a4a174d65c524547eb4b5e193
Won SeungBin <wsb0514@naver.com>
2023-11-21 22:47:49 +0900

    [FEAT] 이미지 로딩 캐싱 및 crop

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/util/SnapPointUtil.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/util/SnapPointUtil.kt
@@ -4,13 +4,20 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import coil.ImageLoader
+import coil.request.CachePolicy
 import coil.request.ImageRequest
 import coil.request.SuccessResult
+import coil.transform.CircleCropTransformation
 
 
-    suspend fun Context.getSnapPointFromUri(uri: String): Bitmap{
-        val request = ImageRequest.Builder(this).data(uri).size(50).build()
-        val result = (ImageLoader(this).execute(request) as SuccessResult).drawable
+suspend fun Context.getSnapPointFromUri(uri: String): Bitmap{
+        val request = ImageRequest.Builder(this)
+            .data(uri)
+            .memoryCachePolicy(CachePolicy.ENABLED)
+            .size(100)
+            .transformations(CircleCropTransformation())
+            .build()
+        val result = ImageLoader(this).execute(request).drawable
         return (result as BitmapDrawable).bitmap
     }
 
