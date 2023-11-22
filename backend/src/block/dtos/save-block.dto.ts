b6b6e7a6ba3d71a7bfcb6b5cdbef4c1b5f12bea4
Heebeom Yang <89298198+takeny1998@users.noreply.github.com>
2023-11-22 18:51:56 +0900

    [REFACTOR] 중복된 Block DTO 통합

--- backend/src/block/dtos/save-block.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsOptional, IsString } from 'class-validator';
-import { CreateBlockDto } from './create-block.dto';
-
-export class SaveBlockDto extends CreateBlockDto {
-  @IsOptional()
-  @IsString()
-  readonly uuid?: string;
-}
