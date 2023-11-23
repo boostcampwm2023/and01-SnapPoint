cb34f83d69e2631c322f1482832db67224fc72ff
LJG7123 <ljg7123@naver.com>
2023-11-23 15:10:40 +0900

    [FEAT] Bottom Sheet Collapsed 상태에서 다른 Navigation Item 선택 시 HALF_EXPANDED로 상태 변경

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/MainActivity.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/MainActivity.kt
@@ -194,6 +194,9 @@ class MainActivity :
         binding.bnv.setOnItemSelectedListener { menuItem ->
             navController.popBackStack()
             navController.navigate(menuItem.itemId)
+            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED) {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+            }
             true
         }
     }
