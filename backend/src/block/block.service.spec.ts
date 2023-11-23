1e67e6c78a474089a94ec53bb5630b5842babe63
Heebeom Yang <89298198+takeny1998@users.noreply.github.com>
2023-11-23 15:00:26 +0900

    [TEST] 미디어 블록 좌표 검사 테스트 추가
    - 위도, 경도 중 하나라도 없으면 제대로 throw 뱉는지 추가

--- backend/src/block/block.service.spec.ts
+++ backend/src/block/block.service.spec.ts
@@ -43,5 +43,16 @@ describe('BlocksService', () => {
         }),
       ).rejects.toThrow(BadRequestException);
     });
+
+    it('미디어 블록은 위도, 경도 값을 모두 가져야 한다.', async () => {
+      await expect(
+        service.create('589bb04b-9aa3-4a2b-bc66-8ea8235c4c01', {
+          type: 'media',
+          content: 'This is Media Block.',
+          longitude: -74.3538,
+          order: 1,
+        }),
+      ).rejects.toThrow(BadRequestException);
+    });
   });
 });
