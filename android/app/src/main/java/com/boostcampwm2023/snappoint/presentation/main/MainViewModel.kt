18acf7d211aaf7f20a5aa58665eb130936a9bbfc
rbybound <ruviiit@gmail.com>
2023-11-23 23:26:38 +0900

    [FEAT] 목 데이터 추가 개선 (3차) - 구글 본사 주변으로 이동

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/MainViewModel.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/MainViewModel.kt
@@ -53,12 +53,12 @@ class MainViewModel @Inject constructor(
                             ),
                             PostBlockState.IMAGE(
                                 content = "https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg",
-                                position = PositionState(10.001, 10.002),
+                                position = PositionState(37.421793077676774, -122.09180117366115),
                                 description = "고양이입니다.",
                                 address = "null"
                             ),PostBlockState.IMAGE(
                                 content = "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201901/20/28017477-0365-4a43-b546-008b603da621.jpg",
-                                position = PositionState(10.005, 10.006),
+                                position = PositionState(37.41887606344049, -122.0879954078449),
                                 description = "강아징입니다.",
                                 address = "null"
                             ),
@@ -69,13 +69,13 @@ class MainViewModel @Inject constructor(
                             ),
                             PostBlockState.IMAGE(
                                 content = "https://i.namu.wiki/i/Nvsy3_i1lyInOB79UBbcDeR6MocJ4C8TBN8NjepPwqTnojCbb3Xwge9gQXfAGgW74ZA3c3i16odhBLE0bSwgFA.webp",
-                                position = PositionState(10.004, 10.004),
+                                position = PositionState(37.42155682099068, -122.08342886715077),
                                 description = "이것은 악어~",
                                 address = "null"
                             ),
                             PostBlockState.IMAGE(
                                 content = "https://upload.wikimedia.org/wikipedia/commons/4/41/Siberischer_tiger_de_edit02.jpg",
-                                position = PositionState(10.008, 9.998),
+                                position = PositionState(37.4227919844394, -122.08028507548029),
                                 description = "어흥",
                                 address = "null"
                             ),
@@ -91,19 +91,19 @@ class MainViewModel @Inject constructor(
                             ),
                             PostBlockState.IMAGE(
                                 content = "https://upload.wikimedia.org/wikipedia/commons/8/85/Columbina_passerina.jpg",
-                                position = PositionState(10.0, 9.7),
+                                position = PositionState(37.40837052881207, -122.10293026989889),
                                 description = "비둘기야 먹자 구구구구",
                                 address = "address"
                             ),
                             PostBlockState.IMAGE(
                                 content = "https://upload.wikimedia.org/wikipedia/commons/f/f0/Nipponia_nippon_20091230131054.png",
-                                position = PositionState(9.9, 10.1),
+                                position = PositionState(37.40272239693208, -122.06577824456974),
                                 description = "떴따 오기",
                                 address = "address"
                             ),
                             PostBlockState.IMAGE(
                                 content = "https://upload.wikimedia.org/wikipedia/commons/8/82/Watercock_%28Gallicrex_cinerea%29.jpg",
-                                position = PositionState(10.3, 10.5),
+                                position = PositionState(37.414911773823924, -122.0536102126485),
                                 description = "뜸 부기",
                                 address = "address"
                             ),
