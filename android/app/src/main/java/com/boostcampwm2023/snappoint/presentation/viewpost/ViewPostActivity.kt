9ebe64cd747ac357897148681f8c898018b6e115
LJG7123 <ljg7123@naver.com>
2023-11-27 16:17:16 +0900

    [DESIGN] 게시글 열람 화면 레이아웃 작성

---
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/viewpost/ViewPostActivity.kt
@@ -0,0 +1,9 @@
+package com.boostcampwm2023.snappoint.presentation.viewpost
+
+import com.boostcampwm2023.snappoint.R
+import com.boostcampwm2023.snappoint.databinding.ActivityViewPostBinding
+import com.boostcampwm2023.snappoint.presentation.base.BaseActivity
+
+class ViewPostActivity : BaseActivity<ActivityViewPostBinding>(R.layout.activity_view_post) {
+
+}
---
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/viewpost/post/PostFragment.kt
@@ -0,0 +1,9 @@
+package com.boostcampwm2023.snappoint.presentation.viewpost.post
+
+import com.boostcampwm2023.snappoint.R
+import com.boostcampwm2023.snappoint.databinding.FragmentPostBinding
+import com.boostcampwm2023.snappoint.presentation.base.BaseFragment
+
+class PostFragment : BaseFragment<FragmentPostBinding>(R.layout.fragment_post) {
+
+}
