ae34c405497fcdfd18771f534adbb57d78e2aa6e
rbybound <ruviiit@gmail.com>
2023-11-28 15:57:10 +0900

    [FEAT] 로그인 뷰 모델에 loginRepository 주입

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/login/LoginViewModel.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/login/LoginViewModel.kt
@@ -2,12 +2,24 @@ package com.boostcampwm2023.snappoint.presentation.login
 
 import android.util.Patterns
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.boostcampwm2023.snappoint.data.repository.LoginRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
-class LoginViewModel : ViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val loginRepository: LoginRepository
+) : ViewModel() {
 
     private val _loginFormUiState: MutableStateFlow<LoginFormState> = MutableStateFlow(LoginFormState())
     val loginFormUiState: StateFlow<LoginFormState> = _loginFormUiState.asStateFlow()
@@ -29,12 +41,24 @@ class LoginViewModel : ViewModel() {
     }
 
     fun tryLogin() {
-        // TODO
-        _loginFormUiState.update {
-            it.copy(
-                isLoginInProgress = true
-            )
-        }
+        loginRepository.postLogin(loginFormUiState.value.isEmailValid.toString(), loginFormUiState.value.isPasswordValid.toString())
+            .onStart {
+                _loginFormUiState.update {
+                    it.copy(
+                        isLoginInProgress = true
+                    )
+                }
+            }
+            .onEach {
+
+            }
+            .catch {
+
+            }
+            .onCompletion {
+
+            }
+            .launchIn(viewModelScope)
     }
 
     private fun isEmailValid(username: String): Boolean {
