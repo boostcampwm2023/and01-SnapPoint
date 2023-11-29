2a582652afbdd1802095471883c1039b37f444d4
Heebeom Yang <89298198+takeny1998@users.noreply.github.com>
2023-11-29 17:10:37 +0900

    [FIX] File-API 및 Auth 테스트 코드 버그 수정
    - fileAPI의 Bucket Service를 mocking 해, config 정보 없으면 Error 발생하는 부분을 수정
    - Auth 테스트 코드의 서비스 Mocking 방식을 바꿔 위치 예외 수정

--- backend/src/api/file-api/file-api.controller.spec.ts
+++ backend/src/api/file-api/file-api.controller.spec.ts
@@ -5,7 +5,8 @@ import { BucketService } from '@/common/bucket.service';
 import { PrismaProvider } from '@/common/prisma/prisma.provider';
 import { PrismaService } from '@/common/prisma/prisma.service';
 import { FileService } from '@/domain/file/file.service';
-import { ConfigService } from '@nestjs/config';
+import { mockDeep } from 'jest-mock-extended';
+import { mockPrismaProvider } from '@/common/mocks/mock.prisma';
 
 describe('FileApiController', () => {
   let controller: FileApiController;
@@ -13,8 +14,13 @@ describe('FileApiController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FileApiController],
-      providers: [BucketService, PrismaService, PrismaProvider, FileApiService, FileService, ConfigService],
-    }).compile();
+      providers: [BucketService, PrismaService, PrismaProvider, FileApiService, FileService],
+    })
+      .overrideProvider(BucketService)
+      .useValue(mockDeep<BucketService>())
+      .overrideProvider(PrismaProvider)
+      .useValue(mockPrismaProvider)
+      .compile();
 
     controller = module.get<FileApiController>(FileApiController);
   });
