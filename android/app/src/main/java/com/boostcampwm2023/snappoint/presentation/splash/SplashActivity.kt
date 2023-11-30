dd3ffed93b16a011c3aff937c66791ec21c051ed
rbybound <ruviiit@gmail.com>
2023-11-30 15:09:04 +0900

    [FIX] rebase 충돌 해결

---
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/splash/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.boostcampwm2023.snappoint.presentation.splash
+
+import android.os.Bundle
+import com.boostcampwm2023.snappoint.R
+import com.boostcampwm2023.snappoint.databinding.ActivitySplashBinding
+import com.boostcampwm2023.snappoint.presentation.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
