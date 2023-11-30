d5a14c011ae4ed1d98b17fdc972b69eec0c4dbb6
rbybound <ruviiit@gmail.com>
2023-11-30 13:33:41 +0900

    [FEAT] 회원 가입 API 추가

--- android/app/src/main/java/com/boostcampwm2023/snappoint/data/remote/SnapPointApi.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/data/remote/SnapPointApi.kt
@@ -2,11 +2,12 @@ package com.boostcampwm2023.snappoint.data.remote
 
 import com.boostcampwm2023.snappoint.data.remote.model.request.CreatePostRequest
 import com.boostcampwm2023.snappoint.data.remote.model.request.LoginRequest
+import com.boostcampwm2023.snappoint.data.remote.model.request.SignupRequest
 import com.boostcampwm2023.snappoint.data.remote.model.response.CreatePostResponse
 import com.boostcampwm2023.snappoint.data.remote.model.response.ImageResponse
 import com.boostcampwm2023.snappoint.data.remote.model.response.ImageUriResponse
 import com.boostcampwm2023.snappoint.data.remote.model.response.LoginResponse
-import retrofit2.Response
+import com.boostcampwm2023.snappoint.data.remote.model.response.SignupResponse
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -36,4 +37,9 @@ interface SnapPointApi {
 
     @GET("logout")
     suspend fun getLogout()
+
+    @POST("signup")
+    suspend fun postSignup(
+        @Body signupRequest: SignupRequest
+    ): SignupResponse
 }
