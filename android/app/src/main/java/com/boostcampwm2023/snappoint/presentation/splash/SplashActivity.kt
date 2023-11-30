6ffe3c9082dad931b04099fee97b667ebe513dce
rbybound <ruviiit@gmail.com>
2023-11-30 15:49:49 +0900

    [FEAT] onCreate에서 실행

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/splash/SplashActivity.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/splash/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.boostcampwm2023.snappoint.presentation.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -12,7 +13,6 @@ import com.boostcampwm2023.snappoint.presentation.base.BaseActivity
 import com.boostcampwm2023.snappoint.presentation.login.LoginActivity
 import com.boostcampwm2023.snappoint.presentation.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -24,16 +24,12 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
         super.onCreate(savedInstanceState)
 
         collectViewModelEvent()
-    }
-
-    override fun onResume() {
-        super.onResume()
         viewModel.login()
     }
 
     private fun collectViewModelEvent() {
         lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
                 viewModel.event.collect { event ->
                     when(event) {
                         is SplashEvent.Success -> {
