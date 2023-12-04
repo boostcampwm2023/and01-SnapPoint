b3038ea1ce287f9d6bac4d77dbc00a11ea60a189
joojae02 <jake010417@gmail.com>
2023-12-04 19:25:53 +0900

    [FEATURE]: RedisServcie 구현, sets 관련 함수 구현

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,7 +6,6 @@ import { PrismaModule } from './common/prisma/prisma.module';
 import { PrismaService } from './common/prisma/prisma.service';
 import { PrismaProvider } from './common/prisma/prisma.provider';
 import { UserModule } from './domain/user/user.module';
-import { AuthModule } from './api/auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RefreshTokenService } from './domain/refresh-token/refresh-token.service';
 import { RefreshTokenModule } from './domain/refresh-token/refresh-token.module';
@@ -14,16 +13,15 @@ import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 import { ApiModule } from './api/api.module';
-import { RedisCacheModule } from './common/redis/redis-cache.module';
-import { RedisModule } from '@liaoliaots/nestjs-redis';
 import { validationPipe } from './common/pipes/validation.pipe';
+import { RedisModule } from '@liaoliaots/nestjs-redis';
+import { RedisCacheModule } from './common/redis/redis-cache.module';
 
 @Module({
   imports: [
     BlocksModule,
     PostModule,
     UserModule,
-    AuthModule,
     FileModule,
     PrismaModule,
     ConfigModule.forRoot({
