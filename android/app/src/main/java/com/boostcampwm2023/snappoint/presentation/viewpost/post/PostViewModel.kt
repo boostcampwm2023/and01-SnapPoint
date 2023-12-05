22c00ceb4a30b131e7a68b2225375ab0fd5d8cf8
rbybound <ruviiit@gmail.com>
2023-12-05 16:29:08 +0900

    [FEAT] View Post에 RoomRepository 주입

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/viewpost/post/PostViewModel.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/viewpost/post/PostViewModel.kt
@@ -1,6 +1,7 @@
 package com.boostcampwm2023.snappoint.presentation.viewpost.post
 
 import androidx.lifecycle.ViewModel
+import com.boostcampwm2023.snappoint.data.repository.RoomRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
@@ -9,7 +10,9 @@ import kotlinx.coroutines.flow.asSharedFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class PostViewModel @Inject constructor() : ViewModel() {
+class PostViewModel @Inject constructor(
+    private val roomRepository: RoomRepository
+) : ViewModel() {
 
     private val _event: MutableSharedFlow<PostEvent> = MutableSharedFlow(
         extraBufferCapacity = 1,
