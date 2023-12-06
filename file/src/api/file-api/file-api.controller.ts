6e6ef3a3b1a604df58bf0c5861cbc083283cb806
joojae02 <jake010417@gmail.com>
2023-12-06 16:10:08 +0900

    [FEATURE]: 파일 여러개 필드로 받는 컨트롤러 구현

--- file/src/api/file-api/file-api.controller.ts
+++ file/src/api/file-api/file-api.controller.ts
@@ -8,9 +8,13 @@ import {
   Inject,
   ParseFilePipeBuilder,
   HttpStatus,
+  UploadedFiles,
 } from '@nestjs/common';
 
-import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  FileFieldsInterceptor,
+  FileInterceptor,
+} from '@nestjs/platform-express';
 import { ApiBody, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ClientProxy } from '@nestjs/microservices';
 import { UploadService } from '@/upload/upload.service';
@@ -65,5 +69,52 @@ export class FileApiController {
       { ...uploadedFileDto, userUuid: req.user.uuid },
     );
     return uploadedFileDto;
+  }
+
+  @Post('/video')
+  @ApiOperation({
+    summary: '동영상 업로드 API',
+    description: '파일을 서버에 업로드하고, 파일의 정보를 받는다.',
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        image: {
+          type: 'string',
+          format: 'binary',
+          description: '업로드할 이미지의 바이너리 데이터입니다.',
+        },
+        video: {
+          type: 'string',
+          format: 'binary',
+          description: '업로드할 영상의 바이너리 데이터입니다.',
+        },
+      },
+    },
+  })
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'image', maxCount: 1 },
+        { name: 'video', maxCount: 1 },
+      ],
+      {
+        limits: {
+          fileSize: 1024 * 1024 * 5,
+        },
+      },
+    ),
+  )
+  async uploadVideo(
+    @UploadedFiles()
+    files: {
+      image: Express.Multer.File[];
+      video: Express.Multer.File[];
+    },
+  ) {
+    return await this.uploadService.uploadFileField(files);
   }
 }
