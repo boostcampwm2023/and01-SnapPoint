4719a29c30107f0de8b9ee5d5757d9a4465aef08
rbybound <ruviiit@gmail.com>
2023-12-06 17:45:47 +0900

    [FEAT] Around Fragment 코드 가져오기

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/subscription/SubscriptionFragment.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/subscription/SubscriptionFragment.kt
@@ -3,25 +3,63 @@ package com.boostcampwm2023.snappoint.presentation.main.subscription
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.boostcampwm2023.snappoint.R
 import com.boostcampwm2023.snappoint.databinding.FragmentSubscriptionBinding
 import com.boostcampwm2023.snappoint.presentation.base.BaseFragment
 import com.boostcampwm2023.snappoint.presentation.createpost.CreatePostActivity
+import com.boostcampwm2023.snappoint.presentation.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SubscriptionFragment : BaseFragment<FragmentSubscriptionBinding>(R.layout.fragment_subscription) {
 
     private val viewModel: SubscriptionViewModel by viewModels()
+    private val mainViewModel: MainViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initBinding()
+
+        collectViewModelData()
     }
 
     private fun initBinding() {
+        with(binding) {
+            vm = viewModel
+        }
+    }
 
+    private fun collectViewModelData() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.event.collect { event ->
+                    when(event) {
+                        is SubscriptionEvent.ShowSnapPointAndRoute -> {
+                            mainViewModel.previewButtonClicked(event.index)
+                        }
+
+                        is SubscriptionEvent.NavigateViewPost -> {
+                            navigateToViewPost(
+                                viewModel.uiState.value.posts[event.index].uuid
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun navigateToViewPost(uuid: String) {
+        val bundle = bundleOf("uuid" to uuid)
+        //findNavController().navigate(R.id.action_subscriptionFragment_to_viewPostActivity)
     }
 }
--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/subscription/SubscriptionViewModel.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/subscription/SubscriptionViewModel.kt
@@ -4,11 +4,20 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.boostcampwm2023.snappoint.data.repository.RoomRepository
+import com.boostcampwm2023.snappoint.presentation.model.PostSummaryState
 import com.boostcampwm2023.snappoint.presentation.util.SignInUtil
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,6 +25,36 @@ class SubscriptionViewModel @Inject constructor(
     private val roomRepository: RoomRepository,
     private val signInUtil: SignInUtil
 ) : ViewModel() {
+
+    private val _uiState: MutableStateFlow<SubscriptionUiState> = MutableStateFlow(
+        SubscriptionUiState(
+            onPreviewButtonClicked = { index -> previewButtonClicked(index) },
+            onViewPostButtonClicked = { index -> viewPostButtonClicked(index) }
+        )
+    )
+    val uiState: StateFlow<SubscriptionUiState> = _uiState.asStateFlow()
+
+    private val _event: MutableSharedFlow<SubscriptionEvent> = MutableSharedFlow(
+        extraBufferCapacity = 1,
+        onBufferOverflow = BufferOverflow.DROP_OLDEST
+    )
+    val event: SharedFlow<SubscriptionEvent> = _event.asSharedFlow()
+
+    fun updatePosts(posts: List<PostSummaryState>) {
+        _uiState.update {
+            it.copy(
+                posts = posts
+            )
+        }
+    }
+
+    private fun previewButtonClicked(index: Int) {
+        _event.tryEmit(SubscriptionEvent.ShowSnapPointAndRoute(index))
+    }
+
+    private fun viewPostButtonClicked(index: Int) {
+        _event.tryEmit(SubscriptionEvent.NavigateViewPost(index))
+    }
 
     fun getSavedPost() {
         roomRepository.getLocalPosts(signInUtil.getEmail())
