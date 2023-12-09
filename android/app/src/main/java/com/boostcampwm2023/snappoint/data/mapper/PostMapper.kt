8557a35cf2f293998521cc98b1e3f7be727c137c
Won SeungBin <wsb0514@naver.com>
2023-12-09 21:42:43 +0900

    [REFACTOR] PostBlockState, PostBlock 및 mapper 수정

--- android/app/src/main/java/com/boostcampwm2023/snappoint/data/mapper/PostMapper.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/data/mapper/PostMapper.kt
@@ -23,14 +23,21 @@ fun PostBlock.asPostBlockState(): PostBlockState {
                     uuid = blockUuid!!,
                     description = this.content,
                     content = this.files[0].url720P!!,
+                    url480P = this.files[0].url480P!!,
+                    url144P = this.files[0].url144P!!,
                     position = this.asPositionState(),
                 )
             } else {
+                val (thumbnail, video) = this.files.partition { it.url.isNullOrEmpty() }
                 PostBlockState.VIDEO(
                     uuid = blockUuid!!,
                     description = this.content,
-                    content = this.files[0].url720P!!,
+                    content = video[0].url!!,
                     position = this.asPositionState(),
+                    thumbnail144P = thumbnail[0].url144P!!,
+                    thumbnail480P = thumbnail[0].url480P!!,
+                    thumbnail720P = thumbnail[0].url720P!!,
+                    thumbnailUuid = thumbnail[0].fileUuid,
                 )
             }
 
