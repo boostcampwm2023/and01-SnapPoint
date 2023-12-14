0194ea30a6ee4d5cd7d83a18a996e05cd4b1ae6a
rbybound <ruviiit@gmail.com>
2023-12-14 10:45:48 +0900

    [FEAT] 프로필 사진 위치에 게시글 첫 번째 미디어 넣기

--- android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/preview/PreviewAdapter.kt
+++ android/app/src/main/java/com/boostcampwm2023/snappoint/presentation/main/preview/PreviewAdapter.kt
@@ -3,8 +3,12 @@ package com.boostcampwm2023.snappoint.presentation.main.preview
 import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.request.CachePolicy
+import coil.transform.CircleCropTransformation
 import com.boostcampwm2023.snappoint.databinding.ItemImagePreviewBinding
 import com.boostcampwm2023.snappoint.presentation.model.PostBlockState
 
@@ -41,4 +45,18 @@ class PreviewAdapter : RecyclerView.Adapter<PreviewViewHolder>() {
 fun RecyclerView.bindRecyclerViewAdapter(blocks: List<PostBlockState>){
     if (adapter == null) adapter = PreviewAdapter()
     (adapter as PreviewAdapter).updateList(blocks)
+}
+
+@BindingAdapter("profile_image")
+fun ImageView.bindingProfileImage(blocks: List<PostBlockState>) {
+    val mediaBlock = blocks.firstOrNull { it !is PostBlockState.TEXT } ?: return
+    val profileUrl: String = when (mediaBlock) {
+        is PostBlockState.IMAGE -> mediaBlock.url144P
+        is PostBlockState.VIDEO -> mediaBlock.thumbnail144P
+        else -> return
+    }
+    this.load(profileUrl) {
+        memoryCachePolicy(CachePolicy.ENABLED)
+        transformations(CircleCropTransformation())
+    }
 }
