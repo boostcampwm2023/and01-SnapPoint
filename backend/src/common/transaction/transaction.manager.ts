6429d78c6c2e06738d67580a2f7a08ae76e89483
Heebeom Yang <89298198+takeny1998@users.noreply.github.com>
2023-12-19 01:00:27 +0900

    [REFACTOR] `PrismaManager` 주석 추가 및 리팩토링
    - PrismaManager 클래스에 JSDoc과 중요 부분 주석 추가
    - 메소드 리팩토링 수행

--- backend/src/common/transaction/transaction.manager.ts
+++ backend/src/common/transaction/transaction.manager.ts
@@ -4,10 +4,11 @@ import { DiscoveryService, MetadataScanner, Reflector } from '@nestjs/core';
 import { PrismaService } from '../prisma/prisma.service';
 import { AsyncLocalStorage } from 'async_hooks';
 import { TRANSACTIONAL_KEY } from './transacion.key';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class TransactionManager implements OnModuleInit {
-  private readonly asyncLocalStorage = new AsyncLocalStorage<any>();
+  private readonly asyncLocalStorage = new AsyncLocalStorage<{ txClient: Prisma.TransactionClient }>();
 
   constructor(
     private readonly discoverService: DiscoveryService,
@@ -16,11 +17,18 @@ export class TransactionManager implements OnModuleInit {
     private readonly prisma: PrismaService,
   ) {}
 
+  /**
+   * Initializes the transaction manager, wrapping methods and repositories with transactional logic.
+   */
   onModuleInit() {
     this.wrapDecorators();
     this.wrapRepository();
   }
 
+  /**
+   * Retrieves static instances from the discovery service.
+   * @returns Array of static instances.
+   */
   private getStaticInstances(): any[] {
     return this.discoverService
       .getProviders()
@@ -28,33 +36,46 @@ export class TransactionManager implements OnModuleInit {
       .map((wrapper) => wrapper.instance);
   }
 
+  /**
+   * Wraps a method with transactional logic.
+   * @param method The original method to wrap.
+   * @param instance The instance of the class containing the method.
+   * @returns The wrapped method.
+   */
   private wrapMethods(method: any, instance: any) {
     const { prisma, asyncLocalStorage } = this;
 
     return async function (...args: any[]) {
-      const result = await prisma.$transaction(async (txClient) =>
-        asyncLocalStorage.run({ txClient }, async () => {
-          return method.apply(instance, args);
-        }),
+      // Wrapping the original method with transaction logic.
+      return await prisma.$transaction(async (txClient) =>
+        asyncLocalStorage.run({ txClient }, () => method.apply(instance, args)),
       );
-      return result;
     };
   }
 
+  /**
+   * Wraps methods annotated with @Transactional.
+   */
   wrapDecorators() {
     const instances = this.getStaticInstances();
 
     instances.forEach((instance) => {
-      const names = this.metadataScanner.getAllMethodNames(Object.getPrototypeOf(instance));
+      const prototype = Object.getPrototypeOf(instance);
+      const methodNames = this.metadataScanner.getAllMethodNames(prototype);
 
-      names.forEach((name) => {
+      methodNames.forEach((name) => {
         const method = instance[name];
-        if (!this.reflector.get(TRANSACTIONAL_KEY, method)) return;
-        instance[name] = this.wrapMethods(method, instance);
+        // Wrap only if the method is annotated with @Transactional.
+        if (this.reflector.get(TRANSACTIONAL_KEY, method)) {
+          instance[name] = this.wrapMethods(method, instance);
+        }
       });
     });
   }
 
+  /**
+   * Wraps repositories to use the transactional client from AsyncLocalStorage.
+   */
   wrapRepository() {
     const { asyncLocalStorage } = this;
     const instances = this.getStaticInstances();
@@ -62,14 +83,14 @@ export class TransactionManager implements OnModuleInit {
     instances
       .filter((instance) => instance instanceof Repository)
       .forEach((instance) => {
-        const { prisma: prevClient } = instance;
+        const prevClient = instance.prisma;
 
+        // Defining a getter to provide the transactional client.
         Object.defineProperty(instance, 'prisma', {
           configurable: false,
           get() {
             const store = asyncLocalStorage.getStore();
-            if (!store) return prevClient;
-            return store.txClient;
+            return store ? store.txClient : prevClient;
           },
         });
       });
