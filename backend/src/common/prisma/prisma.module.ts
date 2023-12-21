3c91d70e2dbfae8acac7a29051af72f9d2bf4eaa
Heebeom Yang <89298198+takeny1998@users.noreply.github.com>
2023-12-21 20:25:27 +0900

    [REFACTOR] 사용하지 않는 Prisma, Repository 확장 제거

--- backend/src/common/prisma/prisma.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from '@/common/prisma/prisma.service';
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
-export class PrismaModule {}
