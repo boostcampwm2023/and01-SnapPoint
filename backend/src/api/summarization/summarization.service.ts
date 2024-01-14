57766c44ffbb2484128765ae82ac5a1beec50d87
takeny1998 <89298198+takeny1998@users.noreply.github.com>
2024-01-14 14:32:39 +0900

    [REFACTOR] Summary Service 리팩토링

--- backend/src/api/summarization/summarization.service.ts
+++ backend/src/api/summarization/summarization.service.ts
@@ -1,6 +1,9 @@
+import { PostService } from './../../domain/post/post.service';
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { firstValueFrom, map } from 'rxjs';
+import { SummaryPostDto } from './dtos/summary-post.dto';
 
 @Injectable()
 export class SummarizationService {
@@ -8,39 +11,68 @@ export class SummarizationService {
   private readonly maxContentLength = 1900;
   private readonly maxSentenceLength = 200;
   private readonly minWordCount = 5;
+  private readonly summaryClientId = this.configService.getOrThrow('NCP_AI_CLIENT_ID');
+  private readonly summaryClientSecret = this.configService.getOrThrow('NCP_AI_CLIENT_SECRET');
 
   constructor(
     private httpService: HttpService,
     private configService: ConfigService,
+    private postService: PostService,
   ) {}
 
-  async summarizeContent(title: string, content: string): Promise<string | null> {
-    try {
-      const response = await this.httpService
-        .post<{ summary: string }>(
-          this.summaryApiUrl,
-          {
-            document: { title, content },
-            option: { language: 'ko', model: 'general', tone: 3, summaryCount: 1 },
-          },
-          {
-            headers: this.getApiHeaders(),
-          },
-        )
-        .toPromise();
-
-      return response!.data.summary;
-    } catch (error) {
-      Logger.error(error.response?.data);
-      return null;
-    }
-  }
-
-  async summarizePost(title: string, blocks: { content: string; type: string }[]): Promise<string> {
+  async summarizePost({ post, blocks }: SummaryPostDto) {
+    const { uuid, title } = post;
     const content = this.prepareContent(blocks);
 
-    const summary: string | null = this.isContentValid(content) ? await this.summarizeContent(title, content) : null;
-    return summary ? summary : this.getFirstTextBlockContent(blocks);
+    // Summary AI의 동작 조건에 맞지 않는 경우, 첫 번째 텍스트 블록 내용을 저장한다.
+    if (!this.isContentValid(content)) {
+      return this.postService.updatePost({
+        where: { uuid },
+        data: { summary: this.getFirstTextBlockContent(blocks) },
+      });
+    }
+
+    const summary$ = this.summarizeContent(title, content);
+
+    // 요약이 되지 않을 경우 (NULL) 첫 번째 텍스트 블록 내용을 저장한다.
+    if (summary$ === null) {
+      return this.postService.updatePost({
+        where: { uuid },
+        data: { summary: this.getFirstTextBlockContent(blocks) },
+      });
+    }
+
+    const { summary } = await firstValueFrom(summary$);
+    return this.postService.updatePost({
+      where: { uuid },
+      data: { summary },
+    });
+  }
+
+  private summarizeContent(title: string, content: string) {
+    const data = {
+      document: { title, content },
+      option: { language: 'ko', model: 'general', tone: 3, summaryCount: 1 },
+    };
+
+    const config = {
+      headers: {
+        'X-NCP-APIGW-API-KEY-ID': this.summaryClientId,
+        'X-NCP-APIGW-API-KEY': this.summaryClientSecret,
+      },
+    };
+
+    try {
+      const summary$ = this.httpService
+        .post<{ summary: string }>(this.summaryApiUrl, data, config)
+        .pipe(map((res) => res.data));
+
+      return summary$;
+    } catch (error) {
+      // API 연결 오류일 경우 NULL을 반환한다.
+      if (error instanceof HttpException) return null;
+      throw error;
+    }
   }
 
   private prepareContent(blocks: { content: string; type: string }[]): string {
@@ -66,12 +98,5 @@ export class SummarizationService {
   private getFirstTextBlockContent(blocks: { content: string; type: string }[]): string {
     const textBlock = blocks.find((block) => block.type === 'text');
     return textBlock ? textBlock.content : blocks[0].content;
-  }
-
-  private getApiHeaders() {
-    return {
-      'X-NCP-APIGW-API-KEY-ID': this.configService.getOrThrow('NCP_AI_CLIENT_ID'),
-      'X-NCP-APIGW-API-KEY': this.configService.getOrThrow('NCP_AI_CLIENT_SECRET'),
-    };
   }
 }
